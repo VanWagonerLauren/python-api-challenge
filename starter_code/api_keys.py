51624debfbabfbd53e4acb079e399b13618a7e0e
VanWagonerLauren <77031397+VanWagonerLauren@users.noreply.github.com>
2021-03-03 20:44:30 -0600

    Compiled list of cities

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5c5a0edb4ef34077224f86b18b5cea76"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAUT3Xz03sEfN0GOOxmPmrenN3-oPHLdso"
